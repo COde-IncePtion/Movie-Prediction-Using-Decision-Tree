b9023986d22a4c1c5f98a66fdebe3cf37627c7d7
Ashish <ashishr350@gmail.com>
2017-04-15 22:47:02 +0530

    pushing the front end for the project

--- index.py
+++ index.py
@@ -1,20 +1,25 @@
-from flask import Flask
-from flask import request
+from flask import Flask , render_template ,request
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<a href='/profile'>press here</a> "
+def home():
+    return render_template("home.html")
 
-@app.route("/profile")
+@app.route("/insert")
 def profile():
-    return "you r in profile section"
+    return render_template("insert.html")
 
+@app.route("/insertmovie",methods=['POST'])
+def insertmovie():
+    # inserting the movies details in the cassandra database
+    name=request.form['mname'];
 
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
